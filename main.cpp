eaec427338d95e8733809b9b0d736814765d400b
artemjackson <artemjackson95@gmail.com>
2013-04-15 14:55:16 +0300

    Final release

--- main.cpp
+++ main.cpp
@@ -5,10 +5,51 @@ using namespace std;
 
 int main()
 {
-    String a(7);
+    String a("Hello ");
+    cout<<"String class 'a': "<<a<<endl;
 
-    String b(5);
+    String b("World!");
+    cout<<"String class 'b': "<<b<<endl;
 
+    String c;
+    c=a+b;
+    cout<<"String class 'c=a+b': "<<c<<endl<<endl;;
+    cout<<"Length of string 'c' "<<c.length()<<endl;
+    String g("Hello World!");
+    cout<<"String class 'g': "<<g<<endl;
+    cout<<"Substring with position 6 & 15 elements after: "<<g.substr(6,15)<<endl<<endl;
+
+    cout<<"Some comparisons: "<<endl;
+    if(a>=b)
+        cout<<a<<" >= "<<b<<endl;
+    else
+        cout<<a<<" not >= "<<b<<endl;
+
+    if(a<=b)
+        cout<<a<<" <= "<<b<<endl;
+    else
+        cout<<a<<" not <= "<<b<<endl;
+
+    if(a>b)
+        cout<<a<<" > "<<b<<endl;
+    else
+        cout<<a<<" not > "<<b<<endl;
+
+    if(a<b)
+        cout<<a<<" < "<<b<<endl;
+    else
+        cout<<a<<" not < "<<b<<endl;
+
+    if(a==b)
+        cout<<a<<" == "<<b<<endl;
+    else
+        cout<<a<<" not == "<<b<<endl;
+        cout<<endl;
+
+    String f(5);
+    cout<<"Finaly actin: input strung with 5 symbols: ";
+    cin>>f;
+    cout<<"Your sting: "<<f<<endl;
 
     return 0;
 }
--- newstring.h
+++ newstring.h
@@ -1,53 +1,37 @@
 #ifndef NEWSTRING_H_INCLUDED
 #define NEWSTRING_H_INCLUDED
 
+#include <iostream>
+
 class String
 {
+
 public:
     String();
     String(int n);
-    ~String();
+    String(const char *str);
     String(const String &original);
+    ~String();
 
-    String &operator=(String const &original)
-    {
-        if( this == &original )
-            return *this;
-        delete []array;
-        size=original.size;
-        array= new char [size];
-        for(int i=0; i<size; ++i)
-            array[i]=original.array[i];
-        return *this;
-    };
+    int length();
+    int length(const char *str);
+    String substr(int first, int n);
 
-    String operator+(String const&x)
-    {
-        String y(size+x.size);
+    String &operator=(const String &original);
+    String operator+(String x);
 
-        for(int i=0; i<size; ++i)
-            y.array[i]=array[i];
+    bool operator==(String x);
+    bool operator>=(String x);
+    bool operator<=(String x);
+    bool operator>(String x);
+    bool operator<(String x);
+    void operator<<(String x);
+    friend std::ostream &operator<<(std::ostream &output, const String &a);
+    friend std::istream &operator>>(std::istream &input, const String &a);
 
-        for(int i=size; i<x.size; ++i)
-            y.array[i]=x.array[i];
-
-        return y;
-    };
-
-    bool operator==(String const&x)
-    {
-        int total = size>x.size?size:x.size;
-
-        for(int i=0; i<total; ++i)
-            if(array[i]==x.array[i])
-                ++i;
-            else return false;
-        return true;
-    };
 private:
     int size;
     char *array;
 };
-
 
 #endif // NEWSTRING_H_INCLUDED
